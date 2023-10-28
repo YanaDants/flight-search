7b6fe4cd6173e80bed26c1e1d22f5fd440b21e5d
YanaDants <115608585+YanaDants@users.noreply.github.com>
2023-10-28 19:47:08 +0800

    Create MainActivity.kt

---
+++ MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.aviasales
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val cities = resources.getStringArray(R.array.cities)
+    }
+
+
+}
